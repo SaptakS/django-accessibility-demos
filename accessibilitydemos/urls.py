628f39c44e8f2e2b9c463b1c866968d14e201d98
Saptak S <saptak013@gmail.com>
2024-05-31 12:54:06 +0530

    Removes other apps and makes a single app

--- accessibilitydemos/urls.py
+++ accessibilitydemos/urls.py
@@ -21,7 +21,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("images/", include("images.urls")),
+    path("", include("gallery.urls")),
     path("admin/", admin.site.urls),
 ]
 
